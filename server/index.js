89221a1879b57a0f55b6b35045bd24ab8461a12d
Jamal Ahmed <j8ahmed@gmail.com>
2018-10-07 11:01:02 -0400

    updated to test the auction-app server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const socket = require('socket.io');
+
+//App Setup
+const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//create a server
+
+let server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+//Static Files
+app.use(express.static('../'));
+
+
+//Socket setup
+let io = socket(server);
+
+io.on('connection', (socket) =>{
+    console.log(socket.id);
+
+    //listen to bids from the bidder
+    socket.on('bid', (data) => {
+        /*send the data sent from one client to all the other client connect through other sockets*/ 
+        io.emit('bid', data);
+    });
+
+    //listen to the starting price from the Auction Clerk
+    socket.on('auctionStart', (data) => {
+        socket.broadcast.emit('auctionStart', data);
+    });
+});
